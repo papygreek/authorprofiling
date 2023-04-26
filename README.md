797e8b4dfa9926c36df513aba614964bfa2a79cb
Erik <eekihenriksson@gmail.com>
2023-04-26 11:01:27 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Author profiling scripts
+
--- install.sh
+++ install.sh
@@ -3,5 +3,3 @@ python3 -m venv .venv.nosync --prompt authorprofiling
 source ./.venv.nosync/bin/activate
 pip3 install -r requirements.txt
 chmod +x run.sh
-curl -o ./authorprofiling/fasttext-ancientgreek.bin "https://www.icloud.com/iclouddrive/02aFL71pzyL99vhVr8lI-5THw#fasttext-ancientgreek"
-curl -o ./authorprofiling/word2vec-ancientgreek.bin "https://www.icloud.com/iclouddrive/088L-ppQYAKf1Xb9naw2ecUFA#word2vec-ancientgreek"
