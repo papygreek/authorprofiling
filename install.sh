f8adf113c6a811445a19ded29f60c30b7c6d9ced
Erik <eekihenriksson@gmail.com>
2023-05-02 11:49:41 +0300

    update

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 python3 -m venv .venv.nosync --prompt authorprofiling
 source ./.venv.nosync/bin/activate
 pip3 install -r requirements.txt
+pip3 uninstall -y adjusttext
+pip3 install https://github.com/Phlya/adjustText/archive/master.zip
 chmod +x run.sh
